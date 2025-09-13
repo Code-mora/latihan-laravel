94ed33d38a0f0dc33aef82bb8bdfcaba691840b1
Ahwaz-coder <hafizuddinahwaz790@gmail.com>
2025-09-13 10:40:34 +0700

    create-mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return "Hello world dari Laravel";
-});
-
-Route::get('/nama', function () {
-    return "Hi, kenalin nama Aku Udin";
-});
+Route::get('/mahasiswa', [MahasiswaController:: class, 'index']);
+Route::post('/mahasiswa', [MahasiswaController:: class, 'store']);

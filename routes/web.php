52f826ec2f8dff057be7e54408407197d82c8485
Ahwaz-coder <hafizuddinahwaz790@gmail.com>
2025-09-20 09:52:05 +0700

    membuat login dengan breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
-use App\Http\Controllers\MatkulController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/matkul', [MatkulController:: class, 'index']);
-Route::post('/matkul', [MatkulController:: class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

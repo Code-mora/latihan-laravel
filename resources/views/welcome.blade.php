0c889d6ac45e14acaff3f65461d88ac56a631bbe
ShizukanaSeiza <shizukanaseiza765@gmail.com>
2025-12-06 11:45:28 +0700

    landing page new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -127,7 +127,7 @@
                 </p>
             </div>
             <div>
-                <img src="{{ asset('storage/image/landing/mahasiswa-lp3i.png') }}" class="rounded-xl shadow-lg"/>
+                <img src="{{ asset('uploads/landing/mahasiswa-lp3i.png') }}" class="rounded-xl shadow-lg"/>
             </div>
         </div>
     </section>
